9c04e36acd6c1796cd4efae5d7ff0afa0307474f
TheTroyaFake <att0004@alu.medac.es>
2022-02-14 23:34:33 +0100

    subida

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
 
 //settings
 app.set('port', process.env.PORT || 3000);
 
 //middlewares
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(morgan('dev'));
+app.use(cors());
 
 //routes
 app.use(require('./routes/usuarios'))
